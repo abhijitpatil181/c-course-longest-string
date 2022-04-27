739b27639652e0a5ecbaac80973494c82467cf18
abhijit181 <abhijit.patil@cctech.co.in>
2022-04-27 08:34:17 +0530

    add longest_string.c

--- longest_string.c
+++ longest_string.c
@@ -2,19 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc,char* argv[]){
+int main(int argc, char *argv[])
+{
     int index;
-    if (argc<=1){
-        puts("Empty string");
+    if (argc <= 1)
+    {
+        puts("empty string");
     }
-    else{
-        index=1;
-        for(int i=2;i<argc;i++){
-            if(strlen(argv[index])<strlen(argv[i])){
-                index=i;
+    else
+    {
+        index = 1;
+        for (int i = 2; i < argc; i++)
+        {
+            if (strlen(argv[index]) < strlen(argv[i]))
+            {
+                index = i;
             }
         }
-        printf("%s\n",argv[index]);
+        puts(argv[index]);
     }
     return EXIT_SUCCESS;
 }
